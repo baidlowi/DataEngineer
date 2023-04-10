8256eeb3a2db680b6c13a4a23c990f209f127d94
baidlowi <79616397+baidlowi@users.noreply.github.com>
2023-04-11 00:48:31 +0700

    up

--- Tourism Destination Insight/1-terraform/variables.tf
+++ Tourism Destination Insight/1-terraform/variables.tf
@@ -1,5 +1,5 @@
 locals {
-  data_lake_bucket = "de_data_lake"
+  data_lake_bucket = "data-trends"
 }
 
 variable "project" {
@@ -20,5 +20,5 @@ variable "storage_class" {
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
   type = string
-  default = "trips_data_all"
+  default = "data-trends"
 }
